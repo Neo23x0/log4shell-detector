9f9b1e5804011187652f8e2d34210a347e5b19c2
Florian Roth <venom14@gmail.com>
2021-12-12 10:55:15 +0100

    refactor: OOP, pytest

--- log4shell-detector.py
+++ log4shell-detector.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 __author__ = "Florian Roth"
-__version__ = "0.5"
+__version__ = "0.7"
 __date__ = "2021-12-11"
 
 import os
@@ -13,99 +13,150 @@ import argparse
 from datetime import datetime, timedelta
 import traceback
 
-DETECTION_STRINGS = ['${jndi:ldap:', '${jndi:rmi:/', '${jndi:ldaps:/', '${jndi:dns:/']
 DEFAULT_PATHS = ['/var/log', '/storage/log/vmware', '/var/atlassian/application-data/jira/log']
 
-def check_line(line, detection_pad):
-    line = urllib.parse.unquote(line)
-    linechars = list(line)
-    # temporary detection pad
-    dp = copy.deepcopy(detection_pad)
-    # Walk over characters
-    for c in linechars:
-        for detection_string in dp:
-            # If the character in the line matches the character in the detection
-            if c == dp[detection_string]["chars"][dp[detection_string]["level"]]:
-                dp[detection_string]["level"] += 1
-                dp[detection_string]["current_distance"] = 0
-            # If level > 0 count distance to the last char
-            if dp[detection_string]["level"] > 0:
-                dp[detection_string]["current_distance"] += 1
-                # If distance is too big, reset level to zero
-                if dp[detection_string]["current_distance"] > dp[detection_string]["maximum_distance"]:
-                   dp[detection_string]["current_distance"] = 0
-                   dp[detection_string]["level"] = 0 
-            # Is the pad completely empty?
-            if len(dp[detection_string]["chars"]) == dp[detection_string]["level"]:
-                return detection_string
 
-def scan_path(path, detection_pad, quick, debug):
-    number_of_detections = 0
-    # Loop over files
-    for root, directories, files in os.walk(path, followlinks=False):
-        for filename in files:
-            file_path = os.path.join(root, filename)
-            if debug:
-                print("[.] Processing %s ..." % file_path)
-            try:
-                # Gzipped logs
-                if file_path.endswith(".log.gz"):
-                    with gzip.open(file_path, 'rt') as gzlog:        
-                        c = 0
-                        for line in gzlog: 
-                            c += 1
-                            # Quick mode - timestamp check
-                            if quick and not "2021" in line and not "2022" in line:
-                                continue 
-                            # Analyze the line  
-                            result = check_line(line.lower(), detection_pad)
-                            if result:
-                                number_of_detections += 1
-                                print("[!!!] Exploitation attempt detected FILE: %s LINE_NUMBER: %d LINE: %s DEOBFUSCATED_STRING: %s" % 
-                                (file_path, c, line.rstrip(), result))
-                # Plain Text
-                else:
-                    with open(file_path, 'r') as logfile:
-                        c = 0
-                        for line in logfile:
-                            c += 1
-                            # Quick mode - timestamp check
-                            if quick and not "2021" in line and not "2022" in line:
-                                continue
-                            # Analyze the line
-                            result = check_line(line.lower(), detection_pad)
-                            if result:
-                                number_of_detections += 1
-                                print("[!!!] Exploitation attempt detected FILE: %s LINE_NUMBER: %d LINE: %s DEOBFUSCATED_STRING: %s" % 
-                                (file_path, c, line.rstrip(), result))
-            except UnicodeDecodeError as e:
-                if args.debug:
-                    print("[E] Can't process FILE: %s REASON: most likely not an ASCII based log file" % file_path)
-            except Exception as e:
-                print("[E] Cant proces FILE: %s REASON: %s" % (file_path, traceback.print_exc()))
-    # Result
-    if number_of_detections > 0:
-        print("[!] %d exploitation attempts detected in PATH: %s" % (number_of_detections, path))
-    else:
-        print("[+] No Log4Shell exploitation attempts detected in path PATH: %s" % path)
-    return number_of_detections
+class Log4ShellDetector(object):
 
-def prepare_detections(maximum_distance):
-    detection_pad = {}
-    for ds in DETECTION_STRINGS:
-        detection_pad[ds] = {}
-        detection_pad[ds] = {
-            "chars": list(ds),
-            "maximum_distance": maximum_distance,
-            "current_distance": 0,
-            "level": 0
-        }
-    return detection_pad
+    # These strings will be transformed into detection pads
+    DETECTION_STRINGS = ['${jndi:ldap:', '${jndi:rmi:/', '${jndi:ldaps:/', '${jndi:dns:/']
+    # These strings will be applied as they are
+    PLAIN_STRINGS = {
+        " header with value of BadAttributeValueException: ": "https://gist.github.com/Neo23x0/e4c8b03ff8cdf1fa63b7d15db6e3860b#gistcomment-3991502",
+        "at java.naming/com.sun.jndi.url.ldap.ldapURLContext.lookup(": "https://gist.github.com/Neo23x0/e4c8b03ff8cdf1fa63b7d15db6e3860b#gistcomment-3991700",
+    }
+
+    def __init__(self, maximum_distance, debug, quick):
+        self.prepare_detections(maximum_distance)
+        self.debug = debug
+        self.quick = quick
+
+    def decode_line(self, line):
+        while "%" in line:
+            line_before = line
+            line = urllib.parse.unquote(line)
+            if line == line_before:
+                break
+        return line
+
+    def check_line(self, line):
+        # Decode Line
+        decoded_line = self.decode_line(line)
+
+        # Plain Detection
+        for s in self.PLAIN_STRINGS:
+            if s in line or s in decoded_line:
+                return s
+
+        # Detection Pad based Detection
+        # Preparation
+        decoded_line = decoded_line.lower()
+        linechars = list(decoded_line)
+        # temporary detection pad
+        dp = copy.deepcopy(self.detection_pad)
+        # Walk over characters
+        for c in linechars:
+            for detection_string in dp:
+                # If the character in the line matches the character in the detection
+                if c == dp[detection_string]["chars"][dp[detection_string]["level"]]:
+                    dp[detection_string]["level"] += 1
+                    dp[detection_string]["current_distance"] = 0
+                # If level > 0 count distance to the last char
+                if dp[detection_string]["level"] > 0:
+                    dp[detection_string]["current_distance"] += 1
+                    # If distance is too big, reset level to zero
+                    if dp[detection_string]["current_distance"] > dp[detection_string]["maximum_distance"]:
+                        dp[detection_string]["current_distance"] = 0
+                        dp[detection_string]["level"] = 0 
+                # Is the pad completely empty?
+                if len(dp[detection_string]["chars"]) == dp[detection_string]["level"]:
+                    return detection_string
+
+    def scan_file(self, file_path):
+        matches_in_file = []
+        try:
+            # Gzipped logs
+            if file_path.endswith(".log.gz"):
+                with gzip.open(file_path, 'rt') as gzlog:        
+                    c = 0
+                    for line in gzlog: 
+                        c += 1
+                        # Quick mode - timestamp check
+                        if self.quick and not "2021" in line and not "2022" in line:
+                            continue 
+                        # Analyze the line  
+                        result = self.check_line(line)
+                        if result:
+                            matches_dict = {
+                                "line_number": c,
+                                "match_string": result,
+                                "line": line.rstrip()
+                            }
+                            matches_in_file.append(matches_dict)
+            # Plain Text
+            else:
+                with open(file_path, 'r') as logfile:
+                    c = 0
+                    for line in logfile:
+                        c += 1
+                        # Quick mode - timestamp check
+                        if self.quick and not "2021" in line and not "2022" in line:
+                            continue
+                        # Analyze the line
+                        result = self.check_line(line)
+                        if result:
+                            matches_dict = {
+                                "line_number": c,
+                                "match_string": result,
+                                "line": line.rstrip()
+                            }
+                            matches_in_file.append(matches_dict)
+        except UnicodeDecodeError as e:
+            if self.debug:
+                print("[E] Can't process FILE: %s REASON: most likely not an ASCII based log file" % file_path)
+        except Exception as e:
+            print("[E] Cant proces FILE: %s REASON: %s" % (file_path, traceback.print_exc()))
+
+        return matches_in_file
+
+    def scan_path(self, path):
+        number_of_detections = 0
+        # Loop over files
+        for root, directories, files in os.walk(path, followlinks=False):
+            for filename in files:
+                file_path = os.path.join(root, filename)
+                if self.debug:
+                    print("[.] Processing %s ..." % file_path)
+                matches_found = self.scan_file(file_path)
+                if len(matches_found) > 0:
+                    for m in matches_found:
+                        print("[!!!] Exploitation attempt detected FILE: %s LINE_NUMBER: %d LINE: %s DEOBFUSCATED_STRING: %s" % 
+                            (file_path, m["line_number"], m["line"], m["match_string"]))
+                    number_of_detections += 1
+
+        # Result
+        if number_of_detections > 0:
+            print("[!] %d files with exploitation attempts detected in PATH: %s" % (number_of_detections, path))
+        else:
+            print("[+] No files with exploitation attempts detected in path PATH: %s" % path)
+        return number_of_detections
+
+    def prepare_detections(self, maximum_distance):
+        self.detection_pad = {}
+        for ds in self.DETECTION_STRINGS:
+            self.detection_pad[ds] = {}
+            self.detection_pad[ds] = {
+                "chars": list(ds),
+                "maximum_distance": maximum_distance,
+                "current_distance": 0,
+                "level": 0
+            }
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Log4Shell Exploitation Detectors')
     parser.add_argument('-p', nargs='+', help='Path to scan', metavar='path', default='')
+    parser.add_argument('-f', nargs='+', help='File to scan', metavar='path', default='')
     parser.add_argument('-d', help='Maximum distance between each character', metavar='distance', default=30)
     parser.add_argument('--quick', action='store_true', default=False, help="Skip log lines that don't contain a 2021 or 2022 time stamp")
     parser.add_argument('--defaultpaths', action='store_true', default=False, help='Scan a set of default paths that should contain relevant log files.')
@@ -115,40 +166,54 @@ if __name__ == '__main__':
     
     print("     __             ____ ______       ____  ___      __          __          ")
     print("    / /  ___  ___ _/ / // __/ /  ___ / / / / _ \\___ / /____ ____/ /____  ____")
-    print("   / /__/ _ \/ _ `/_  _/\ \/ _ \/ -_) / / / // / -_) __/ -_) __/ __/ _ \/ __/")
+    print("   / /__/ _ \\/ _ `/_  _/\\ \\/ _ \\/ -_) / / / // / -_) __/ -_) __/ __/ _ \\/ __/")
     print("  /____/\\___/\\_, / /_//___/_//_/\\__/_/_/ /____/\\__/\\__/\\__/\\__/\\__/\\___/_/ ")  
     print("            /___/                                                            ")
     print(" ")
     print("  Version %s, %s" % (__version__, __author__))
     
-    if not args.p and not args.defaultpaths:
+    if not args.p and not args.defaultpaths and not args.f:
         parser.print_help(sys.stderr)
         print("")
-        print("[E] You have to select at least one folder to scan with -p target-folder or use --defaultpaths")
+        print("[E] You have to select at least one folder to scan with -p target-folder, a file with -f file or use --defaultpaths")
         sys.exit(1)
     
     print("")
     date_scan_start = datetime.now()
     print("[.] Starting scan DATE: %s" % date_scan_start)
     
-    # Prepare the detection pads
-    detection_pad = prepare_detections(int(args.d))
+    # Create Log4Shell Detector Object
+    l4sd = Log4ShellDetector(maximum_distance=args.d, debug=args.debug, quick=args.quick)
     
     # Counter
     all_detections = 0
     
+    # Scan file
+    if args.f:
+        files = args.f 
+        for f in files:
+            if not os.path.isfile(f):
+                print("[E] File %s doesn't exist" % f)
+                continue
+            print("[+] Scanning FILE: %s ..." % f)
+            matches_found = l4sd.scan_file(f)
+            if len(matches_found) > 0:
+                for m in matches_found:
+                    print("[!!!] Exploitation attempt detected FILE: %s LINE_NUMBER: %d LINE: %s DEOBFUSCATED_STRING: %s" % 
+                        (f, m["line_number"], m["line"], m["match_string"]))
     # Scan paths
-    paths = args.p
-    if args.defaultpaths:
-        paths = DEFAULT_PATHS
-    for path in paths:
-        if not os.path.isdir(path):
-            if not args.defaultpaths:
-                print("[E] Path %s doesn't exist" % path)
-            continue
-        print("[+] Scanning FOLDER: %s ..." % path)
-        detections = scan_path(path, detection_pad, args.quick, args.debug)
-        all_detections += detections
+    else:
+        paths = args.p
+        if args.defaultpaths:
+            paths = DEFAULT_PATHS
+        for path in paths:
+            if not os.path.isdir(path):
+                if not args.defaultpaths:
+                    print("[E] Path %s doesn't exist" % path)
+                continue
+            print("[+] Scanning FOLDER: %s ..." % path)
+            detections = l4sd.scan_path(path)
+            all_detections += detections
 
     # Finish
     if all_detections > 0:
---
+++ tests/test_detection.py
@@ -0,0 +1,14 @@
+import sys, os
+import importlib
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+l4s = importlib.import_module("log4shell-detector", "Log4ShellDetector")
+
+def test_full_path():  
+    l4sd = l4s.Log4ShellDetector(maximum_distance=20, debug=False, quick=False)
+    detections = l4sd.scan_path("./tests")
+    assert detections == 7
+
+def test_url_encoded():  
+    l4sd = l4s.Log4ShellDetector(maximum_distance=20, debug=False, quick=False)
+    matches = l4sd.scan_file("./tests/test-cases/test-url-encoded.log")
+    assert len(matches) == 3
