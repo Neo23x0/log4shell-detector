6dd017e69550260344e3659b13f8a66c3efa518c
m0n4 <unholyman@hotmail.fr>
2021-12-12 19:04:57 +0100

    Update log4shell-detector.py

--- log4shell-detector.py
+++ log4shell-detector.py
@@ -145,7 +145,7 @@ class Log4ShellDetector(object):
                     for m in matches_found:
                         print("[!!!] Exploitation attempt detected FILE: %s LINE_NUMBER: %d LINE: %s DEOBFUSCATED_STRING: %s" % 
                             (file_path, m["line_number"], m["line"], m["match_string"]))
-                        number_of_detections += 1
+                    number_of_detections += 1
 
         # Result
         if number_of_detections > 0:
