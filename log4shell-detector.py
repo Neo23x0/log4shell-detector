e901b4b5dda3b1876e8130bb89d03b8b97b6dd5e
Florian Roth <florian.roth@nextron-systems.com>
2021-12-14 15:02:30 +0100

    fix: -d flag as int, { follows $

--- log4shell-detector.py
+++ log4shell-detector.py
@@ -74,6 +74,11 @@ class Log4ShellDetector(object):
             for detection_string in dp:
                 # If the character in the line matches the character in the detection
                 if c == dp[detection_string]["chars"][dp[detection_string]["level"]]:
+                    # { directly follows $
+                    if dp[detection_string]["level"] == 1 and not dp[detection_string]["current_distance"] == 1:
+                        # if not ${ but $ .... { do a complete reset of the pad evaluation
+                        dp[detection_string]["current_distance"] = 0
+                        dp[detection_string]["level"] = 0 
                     dp[detection_string]["level"] += 1
                     dp[detection_string]["current_distance"] = 0
                 # If level > 0 count distance to the last char
@@ -83,7 +88,7 @@ class Log4ShellDetector(object):
                     if dp[detection_string]["current_distance"] > dp[detection_string]["maximum_distance"]:
                         dp[detection_string]["current_distance"] = 0
                         dp[detection_string]["level"] = 0 
-                # Is the pad completely empty?
+                # Is the pad complete
                 if len(dp[detection_string]["chars"]) == dp[detection_string]["level"]:
                     return detection_string
 
@@ -183,7 +188,7 @@ class Log4ShellDetector(object):
 def evaluate_log_paths():
     paths = []
     print("[.] Automatically evaluating the folders to which apps write logs ...")
-    command = "lsof 2>/dev/null | grep '\.log' | sed 's/.* \//\//g' | sort | uniq"
+    command = "lsof 2>/dev/null | grep '\\.log' | sed 's/.* \\//\\//g' | sort | uniq"
     path_eval = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     output = path_eval.communicate()[0].splitlines()
     for o in output:
@@ -204,7 +209,7 @@ if __name__ == '__main__':
     group.add_argument('-p', nargs='+', help='Path to scan', metavar='path', default='')
     group.add_argument('-f', nargs='+', help='File to scan', metavar='path', default='')
     group.add_argument('--auto', action='store_true', help='Automatically evaluate locations to which logs get written and scan these folders recursively (new default if no path is given)')
-    parser.add_argument('-d', help='Maximum distance between each character', metavar='distance', default=40)
+    parser.add_argument('-d', type=int, help='Maximum distance between each character', metavar='distance', default=40)
     parser.add_argument('--quick', action='store_true', help="Skip log lines that don't contain a 2021 or 2022 time stamp")
     parser.add_argument('--debug', action='store_true', help='Debug output')
     parser.add_argument('--summary', action='store_true', help='Show summary only')
