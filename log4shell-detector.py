c521bee725d85ed6500e560155ddf0acac9c6989
Walid DABOUBI <walid.daboubi@gmail.com>
2021-12-19 01:35:34 +0100

    fixed check_log4j_used
    replace command by checker_command in line 40

--- log4shell-detector.py
+++ log4shell-detector.py
@@ -37,7 +37,7 @@ def check_log4j_used():
         "grep -r --include *.[wj]ar \"JndiLookup.class\" / 2>&1 | grep matches",
     ]
     for checker_command in checker_commands:
-        if len(subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT).communicate()[0].splitlines()) > 0:
+        if len(subprocess.Popen(checker_command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT).communicate()[0].splitlines()) > 0:
             return True
     return False
 
